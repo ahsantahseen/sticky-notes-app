bd6b93e201643d8f733fb704030ded125a547d89
Ahsan Tahseen <58149036+ahsantahseen@users.noreply.github.com>
2020-08-01 21:38:44 +0500

    Second BareBone Commit

--- src/Components/Note/Note.js
+++ src/Components/Note/Note.js
@@ -1,7 +1,13 @@
 import React from "react";
+import classes from "./Note.module.css";
 
 const Note = () => {
-  return <div></div>;
+  return (
+    <div className={classes.Note}>
+      <h3>Title:</h3>
+      <p></p>
+    </div>
+  );
 };
 
 export default Note;
--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Note from "../Components/Note/Note";
 
 import "./App.css";
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <h2>Sticky Notes</h2>
+      <Note></Note>
     </div>
   );
 }
