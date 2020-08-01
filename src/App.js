ad704c20e38fe58aba729e687abe847e640e99e7
Ahsan Tahseen <58149036+ahsantahseen@users.noreply.github.com>
2020-08-01 21:23:11 +0500

    Second BareBone Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h2>Hello World </h2>
+      <h2>Hello World</h2>
     </div>
   );
 }
