774f5ca9451042ccb453f777f175e81842ddc457
Ahsan Tahseen <58149036+ahsantahseen@users.noreply.github.com>
2020-08-01 21:16:31 +0500

    Second BareBone Commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h2>Hello World </h2>
+    </div>
+  );
 }
 
 export default App;
