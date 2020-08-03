3a55da7aa4293e8aa1f7971a5e424dc1c09f19c5
Ahsan Tahseen <58149036+ahsantahseen@users.noreply.github.com>
2020-08-04 01:16:36 +0500

    Version 1.0

--- src/Components/Note/Note.js
+++ src/Components/Note/Note.js
@@ -4,11 +4,12 @@ import Auxilary from "../../hoc/Auxilary";
 
 const Note = (props) => {
   const colors = ["#fff740", "#7afcff", "#ff7eb9", "#feff9c"];
-  let random_color = colors[Math.floor(Math.random() * colors.length)];
+  const random_color = colors[Math.floor(Math.random() * colors.length)];
 
   const positions = ["rotate(5deg)", "rotate(-5deg)"];
 
-  let random_position = positions[Math.floor(Math.random() * positions.length)];
+  const random_position =
+    positions[Math.floor(Math.random() * positions.length)];
 
   console.log(random_color);
   return (
@@ -17,8 +18,8 @@ const Note = (props) => {
         className={classes.sticky}
         style={{ background: random_color, transform: random_position }}
       >
-        <b>Note:</b>
-        <p contentEditable="true">Editable text...</p>
+        <b contentEditable="true">Untitled</b>
+        <p contentEditable="true"></p>
       </div>
     </Auxilary>
   );
