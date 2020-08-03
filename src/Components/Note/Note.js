14d244cd0d8a687f9ed006be250efe1829488cbe
Ahsan Tahseen <58149036+ahsantahseen@users.noreply.github.com>
2020-08-04 00:24:54 +0500

    Function Working CSS WORKING OK! (SOMEONE CAN SORT IT OUT FOR A BETTER EXPERIENCE)

--- src/Components/Note/Note.js
+++ src/Components/Note/Note.js
@@ -3,9 +3,13 @@ import classes from "./Note.module.css";
 import Auxilary from "../../hoc/Auxilary";
 
 const Note = (props) => {
+  const colors = ["#fff740", "#7afcff", "#ff7eb9", "#feff9c"];
+  let random_color = colors[Math.floor(Math.random() * colors.length)];
+
+  console.log(random_color);
   return (
     <Auxilary>
-      <div className={classes.sticky}>
+      <div className={classes.sticky} style={{ background: random_color }}>
         <b>Note:</b>
         <p contentEditable="true">Editable text...</p>
       </div>
