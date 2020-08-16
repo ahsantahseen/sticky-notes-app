bde824f34d3e3c3a3ee91c629d04f08f691d65ce
Ahsan Tahseen <58149036+ahsantahseen@users.noreply.github.com>
2020-08-16 23:26:38 +0500

    Version 1.1 Improvements added

--- src/Components/Note/Note.js
+++ src/Components/Note/Note.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import classes from "./Note.module.css";
 import Auxilary from "../../hoc/Auxilary";
 
 const Note = (props) => {
-  const colors = ["#fff740", "#7afcff", "#ff7eb9", "#feff9c"];
-  let random_color = colors[Math.floor(Math.random() * colors.length)];
+  const [random_color, setrandom_color] = useState();
+  const [random_position, setrandom_position] = useState();
 
-  const positions = ["rotate(5deg)", "rotate(-5deg)"];
+  useEffect(() => {
+    const colors = ["#fff740", "#7afcff", "#ff7eb9", "#feff9c"];
+    setrandom_color(colors[Math.floor(Math.random() * colors.length)]);
 
-  let random_position = positions[Math.floor(Math.random() * positions.length)];
+    const positions = ["rotate(5deg)", "rotate(-5deg)"];
+
+    setrandom_position(positions[Math.floor(Math.random() * positions.length)]);
+  }, []);
 
   return (
     <Auxilary>
